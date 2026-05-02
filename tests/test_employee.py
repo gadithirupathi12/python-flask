65c57aab5368184694339194eefcc9fae8199a03
gadithirupathi12 <gadithirupathi12@gmail.com>
2026-05-02 21:48:41 +0530

    Update test_employee.py

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -2,7 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from conftest import BASE_URL
+from tests.conftest import BASE_URL   ✅
 
 class TestEmployeeDirectory:
 
