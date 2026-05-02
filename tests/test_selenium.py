b953ee00b9c6c54dce4b1d8e6c986c01f6d3e8ae
gadithirupathi12 <gadithirupathi12@gmail.com>
2026-05-02 21:26:17 +0530

    Refactor Selenium test to use pytest fixture
    Refactor test to use pytest fixture for driver setup.

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,16 +1,19 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def test_app_loads():
-    opts = Options()
-    opts.add_argument("--headless=new")
-    opts.add_argument("--no-sandbox")
-    opts.add_argument("--disable-dev-shm-usage")
+VM_IP   = '192.168.1.100'   # Replace with your actual VM IP
+VM_PORT = '5000'
+BASE_URL = f'http://{VM_IP}:{VM_PORT}'
 
-    driver = webdriver.Chrome(options=opts)
-
-    driver.get("http://127.0.0.1:5000")
-
-    assert "Employee" in driver.page_source  # adjust based on your page
-
-    driver.quit()
+@pytest.fixture(scope='module')
+def driver():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    drv = webdriver.Chrome(options=options)
+    drv.implicitly_wait(10)
+    yield drv
+    drv.quit()
